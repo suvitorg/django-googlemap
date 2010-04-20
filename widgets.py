d7f80465ca5c2d3d8c7e8a2eec2fe70646048691
suvit <vsafronovich@gmail.com>
2010-04-20 08:59:12 +0000

    show nice map block

--- widgets.py
+++ widgets.py
@@ -110,7 +110,7 @@ class LocationWidget(forms.widgets.Widget):
             
         ''' % dict(name=name, lat=lat, lng=lng, def_lat=DEFAULT_LATITUDE, def_lng=DEFAULT_LONGTITUDE)
         html = self.inner_widget.render("%s" % name, "%f,%f" % (lat,lng), dict(id='id_%s' % name))
-        html += u"<div id=\"map_%s\" class=\"gmap\" style=\"width: %dpx; height: %dpx\"></div>" % (name, self.map_width, self.map_height)
+        html += u"<div id=\"map_%s\" class=\"gmap\" style=\"width: %dpx; height: %dpx; float: left;\"></div>" % (name, self.map_width, self.map_height)
         html += u'<a href="#" onclick=" return resetPosition_%(name)s()">Сбросить</a>' % dict(name=name)
         return mark_safe(js + html)
 
